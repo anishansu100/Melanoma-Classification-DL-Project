0b8a0b6abad0d7bb17330a2b18a84ac023461fe6
Anishansu Pradhan <anishansu100@gmail.com>
2022-05-05 00:00:21 -0400

    DFSA

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ from keras.preprocessing.image import ImageDataGenerator
 def get_data(file_path):
 	# Initialising the generators for train and test data
 	# The rescale parameter ensures the input range in [0, 1] 
-	train_datagen = ImageDataGenerator(rescale = 1./255, validation_split=0.2)
+	train_datagen = ImageDataGenerator(rescale = 1./255, rotation_range=10, validation_split=0.2)
 	train_generator = train_datagen.flow_from_directory(
                   '/home/anish_pradhan/Melanoma-Classification-DL-Project/train',
                   target_size =(100, 100),  # target_size = input image size
