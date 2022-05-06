797426c1964ef9f718fe64a727eaae1bc8d01bca
Anishansu Pradhan <anishansu100@gmail.com>
2022-05-06 12:31:50 -0400

    FSDA

--- convolution.py
+++ convolution.py
@@ -108,8 +108,7 @@ def main():
             Dense(1,  activation='relu'),
         ])
     model.compile(optimizer= tf.keras.optimizers.Adam(learning_rate = 1e3), loss= tf.keras.losses.BinaryCrossentropy(), metrics = ['BinaryAccuracy', 'AUC'])
-    model.fit(train_generator[0][0:100],train_generator[1],
-        batch_size = 10,
+    model.fit(train_generator[0:100],
         epochs=2)
     # model.fit(train_generator,
     #     batch_size = 500,
